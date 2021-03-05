f40339c827d1a9af7dc8a9640268f231aa4d7258
vanessamarely <vanessamarely@gmail.com>
2021-03-05 13:13:34 -0500

    task and list created

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { BoardRoutingModule } from './board-routing.module';
 import { BoardComponent } from './board/board.component';
+import { ListComponent } from './components/list/list.component';
+import { TaskComponent } from './components/task/task.component';
 
 
 @NgModule({
-  declarations: [BoardComponent],
+  declarations: [BoardComponent, ListComponent, TaskComponent],
   imports: [
     CommonModule,
     BoardRoutingModule
